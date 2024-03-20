6d8ad87476119eb78d5769ebf2fb648747527900
phpdev047 <phpdev047@gmail.com>
2024-03-20 14:11:20 +0530

    chnage

--- src/Components/PrivacyPolicyOther.js
+++ src/Components/PrivacyPolicyOther.js
@@ -2,51 +2,98 @@ import React from "react";
 const PrivacyPolicy = () => {
   return (
     <div className="privacy-policy-container">
-      <h2>Privacy Policy</h2>
+      <h2>Scope of Privacy Policy</h2>
       <p>
-        . We are committed to protecting the privacy and security of the users
-        of our sites. This Privacy Policy ("Policy" or "Privacy Policy") will
-        advise you about our guidelines concerning the use of your personal
-        information, including, without limitation, the reasonable efforts we
-        make to protect your personal information in accordance with these
-        guidelines, and about what choices you have concerning our use of such
-        information. Please read this Policy carefully. We may need to change
-        this Policy from time to time in order to address new issues and reflect
-        changes on our sites. We will post those changes here so that you will
-        always know our Policy regarding what information we gather, how we
-        might use that information and whether we will disclose that information
-        to anyone. Please refer back to this policy regularly.
+        This Policy applies to your use of all the sites and services owned or
+        operated by ConnectE Technology LLC and affiliated companies
+        (collectively "we," "us" or "our"), including www.connectetech.com,
+        registered URL domains owned by the Company and any of our Company
+        social media pages (collectively, the "Sites"). Unless we say
+        otherwise, all references to the Sites in this Policy include all such
+        sites. This Policy does not apply to your use of unaffiliated sites to
+        which any of the Sites only link. This Policy covers only information
+        collected on the Sites and does not cover any information collected
+        offline by us.
       </p>
-      <h3>Personal Identification Information</h3>
+
+      <h2>Personal Identification Information</h2>
       <p>
         We may request personal identification information from you (such as
-        your name, street address, telephone number, credit card number, e-mail
-        address and/or any other identifier that permits the physical or online
-        contacting of you) in connection with your use of, or participation in,
-        membership registration for our Website, our contests, sweepstakes,
-        promotions, games, surveys, forums, subscription registrations, content
-        submissions, chats, bulletin boards, discussion groups, requests for
-        suggestions, visitors' requests for free products or services or
-        additional information (including via e-mail to us) and in connection
-        with other activities, services or resources we make available on any of
-        our Sites. In all these cases, we will collect personal identification
-        information from you only if you voluntarily submit such information to
-        us.
+        your name, street address, telephone number, credit card number,
+        e-mail address and/or any other identifier that permits the physical
+        or online contacting of you) in connection with your use of, or
+        participation in, membership registration for our Website, our
+        contests, sweepstakes, promotions, games, surveys, forums,
+        subscription registrations, content submissions, chats, bulletin
+        boards, discussion groups, requests for suggestions, visitors'
+        requests for free products or services or additional information
+        (including via e-mail to us) and in connection with other activities,
+        services or resources we make available on any of our Sites. In all
+        these cases, we will collect personal identification information from
+        you only if you voluntarily submit such information to us.
       </p>
+
+      <h2>Consent to Transfer</h2>
+      <p>
+        The Sites are operated in the United States. If you are located in the
+        European Union or elsewhere outside of the United States, please be
+        aware that any information you provide to us will be transferred to
+        the United States. By using our website, participating in any of our
+        services and/or providing us with your information, you consent to
+        this transfer.
+      </p>
+
+      <h2>Your Acceptance of These Terms</h2>
+      <p>
+        By using any of the Sites, you signify your acceptance of our Privacy
+        Policy, your promise to comply with the representations you make and
+        contained in the Privacy Policy and your understanding of the scope
+        and limitations of the protections provided under the Privacy Policy
+        and Terms of Service. If you do not agree to these policies, please do
+        not use our Sites. Your continued use of the Sites following the
+        posting of changes to these terms will mean that you accept those
+        changes.
+      </p>
+
+      <h2>Exceptions to Privacy Policy</h2>
+      <ul>
+        <li>
+          We will release specific information about you or your account to comply with any valid legal inquiry or process such as a search warrant, subpoena, statute or court order.
+        </li>
+        <li>
+          We will also release specific information in special cases, such as if there is an attempted breach of the security of the Sites or a physical or property threat to you or others.
+        </li>
+        <li>
+          We may transfer user information, including personally identifiable information, in connection with a corporate merger, consolidation, the sale of related assets or corporate division or other fundamental corporate change.
+        </li>
+        <li>
+          We may provide access to your personally identifiable information to our contractors who are performing services for us in connection with our Sites or the services that you have requested.
+        </li>
+      </ul>
+      <h2> Privacy of Children</h2>
+      <p>We understand that young people need special safeguards and privacy protection. We realize that they may not understand all the provisions of our policy or be able to make thoughtful decisions about the choices that are made available to our adult users. We strongly urge all parents to participate in their children's exploration of the Internet and any online services and to teach their children about protecting their personal information while online. </p>
+      <p>The Sites are not directed to children under the age of 18. We prohibit registration by persons under the age of 18 for all interactive portions of the Sites, including chats and message boards. You agree to abide by any such restrictions and not to help anyone avoid these restrictions.</p>
+
+      <h2>Links to Other Sites</h2>
+      <p>Users will find advertising or other content on our Sites that link to the sites and services of our partners, suppliers, advertisers, sponsors, licensors and other third parties. We do not control the content or links that appear on these sites. In addition, these sites or services, including their content and links, may be constantly changing. These sites and services may have their own privacy policies and customer service policies or no policy (policies) at all. We encourage you to review the privacy policies of any third-party sites or services before providing any of them with your personal information.</p>
       
+      <h2>Security Policy</h2>
+      <p>Protecting your privacy and your information is a top priority. We have taken reasonable measures to secure both your information and our physical premises. We exercise reasonable care in providing safe transmission of your information from your PC to our servers (unless sent by a means outside of our control, such as e-mail). However, we cannot guarantee that our measures will completely bar unauthorized access to your information, and by using the Sites you assume this risk.</p>
+    
+      <h2>Access to/Correction of Information</h2>
       <p>
-      Except as otherwise provided in this Policy, we reasonably endeavor to ensure that we never intentionally disclose any personal identification information about you as an individual user to any third party without having received your permission (through opting in or similar procedures) except as provided for herein or otherwise as permitted or required under law.
+      If you would like to review any personal information we have retained about you in our database, please send us an e-mail request for this information at  admin@connecte.co To help us address your request, please specify which information you wish to review. Once you have reviewed this information, at your request, we will correct personal information about you that you state is erroneous. To request such a correction, please send an e-mail at  admin@connecte.co
       </p>
-      <h3>Security</h3>
-      <p>
-        We take reasonable measures to protect your information from
-        unauthorized access or disclosure.
-      </p>
-      <h3>Changes to This Privacy Policy</h3>
-      <p>
-        We may update our Privacy Policy from time to time. You are advised to
-        review this Privacy Policy periodically for any changes.
-      </p>
+      <p>You should be aware, however, that it is not always possible to completely remove or modify information in our databases and servers, although we will always make reasonable efforts to do so upon your request. Please also understand that due to the volume of traffic on the Sites and correspondence received, we cannot make any representations about the timeliness of the making of such modifications. In addition, we are unable to modify your information in the records of any third party who has been provided with your information in accordance with this policy.</p>
+    
+          <h2>Choice/Opt Out</h2>
+          <p>If you "opt in" to receive information from us or others, you can change your mind later. If, at any time, you would like to stop receiving such information or opt out of a feature, you can send us an email to notify us of your intention. You should be aware, however, that it is not always possible to completely remove or modify information in our databases and servers, although we will always make reasonable efforts to do so upon your request, and we are unable to have your information removed from the records of any third party who has been provided with your information in accordance with this policy.</p>
+          
+          <h2>Consent to Transfer</h2>
+          <p>The Sites are operated in the United States. If you are located in the European Union or elsewhere outside of the United States, please be aware that any information you provide to us will be transferred to the United States. By using our website, participating in any of our services and/or providing us with your information, you consent to this transfer.</p>
+          
+          <h2>Your Acceptance of These Terms</h2>
+          <p>By using any of the Sites, you signify your acceptance of our Privacy Policy, your promise to comply with the representations you make and contained in the Privacy Policy and your understanding of the scope and limitations of the protections provided under the Privacy Policy and Terms of Service. If you do not agree to these policies, please do not use our Sites. Your continued use of the Sites following the posting of changes to these terms will mean that you accept those changes.</p>
     </div>
   );
 };
